3ad67d51291b31017d01402a945d5304f041b60e
ombasa <ombasaherman@gmail.com>
2021-10-07 13:08:57 +0300

    props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 class Contact extends Component {
     render() {
+        const {name, email, phone} = this.props;
         return (
             <div>
-                <h4> James Bond </h4>
+                <h4> {name}</h4>
                 <ul>
-                     <li>Email: jbond@yahoo.com</li>
-                     <li>Phone: 555-555-555</li>
+                     <li>{email}</li>
+                     <li>{phone}</li>
                 </ul>
             </div>
         )
